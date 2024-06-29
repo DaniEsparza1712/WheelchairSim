a311ecba5692229848f9d0304cc06d808c52ca80
unknown <CIAC@CIAC2-005.academic.mixcoac.upmx.mx>
2024-06-28 20:03:36 -0600

    added wheel add torque functionality to right wheel

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -1,22 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Wheel : MonoBehaviour
 {
     private WheelCollider wCollider;
+    [Header("Movement Info")]
+    [SerializeField]
+    private float brakeTorque;
+    [SerializeField]
+    private float torqueSpeed;
+    [SerializeField]
+    private bool spinnable;
+    [Header("External components")]
+    [SerializeField]
+    private Transform wheelModel;
+
+    private Vector3 inputVelocity;
+
+    [SerializeField]
+    private bool rightHand;
+
+    private Vector3 pos;
+    private Quaternion rot;
     // Start is called before the first frame update
     void Start()
     {
+        Physics.IgnoreLayerCollision(6, 7);
+        Physics.IgnoreLayerCollision(0, 7);
         wCollider = GetComponent<WheelCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            wCollider.motorTorque = 10;
-            Debug.Log("Added torque...");
-        }
+        wCollider.GetWorldPose(out pos, out rot);
+        wheelModel.position = pos;
+    }
+
+    public void AddTorque(){
+        InputDevice handDevice;
+        if(rightHand)
+            handDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        else
+            handDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        handDevice.TryGetFeatureValue(CommonUsages.deviceVelocity, out inputVelocity);
+        wCollider.motorTorque = inputVelocity.magnitude * torqueSpeed;
+    }
+
+    public void BrakeTorque(){
+
     }
 }
